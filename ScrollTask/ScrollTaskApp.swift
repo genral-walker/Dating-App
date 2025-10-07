dac6628e5c33d2400887e802c69e82a8c0d80f08
Genral Walker <lukmansanni60@gmail.com>
2025-10-07 22:21:50 +0100

    Initial Commit

---
+++ ScrollTask/ScrollTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollTaskApp.swift
+//  ScrollTask
+//
+//  Created by Genral Walker on 07/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrollTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
